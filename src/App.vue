96b8bcdc7f276684ab1b60886a36876dcc308a4e
GuoJing <1030102852@qq.com>
2020-03-13 16:59:40 +0800

    normalize npm install

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="app">
-    
+    <h2>长城郭靖</h2>
   </div>
 </template>
 
 <script>
+/**
+ * Normalize.css一种现代的HTML5替代CSS重置方法
+ * 使浏览器更一致地渲染所有元素，并符合现代标准。它只针对需要规范化的样式。
+ * 支持的浏览器Chrome，Edge，Firefox ESR +，IE 10 +，Safari 8 +，Opera
+ * 导入方式：import normalize from 'normalize.css'或require('normalize.css')，当然还有传统引用方式和CDN我们就不说了
+ * */
+import 'normalize.css'
 
-  export default {
-    name: 'App'
-  }
+export default {
+  name: 'App'
+}
 </script>
 
 <style>
